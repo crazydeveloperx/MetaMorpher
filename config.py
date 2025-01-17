0817477369e62acc5fafbb5c60d7d7be60e56426
crazydeveloperx <shivanamdev198+1@gmail.com>
2025-01-17 22:32:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,16 +8,17 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = os.environ.get("API_ID", "10811400")
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAEelXXHhNoWXR_duKpDJiFl39LbI7A3sU0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7204693012:AAE3BsJPhgTm4K__kbV_IfHlsBpkrF-tbg0")
 ADMIN = int(os.environ.get("ADMIN", '6469754522'))
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
-DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://INFINITYRENAME24BOT:INFINITYRENAME24BOT@cluster0.colorb9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Crazybotz")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "CloneV2Support")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://FilterCraftXBotFiledata:FilterCraftXBotFiledata@cluster0.b7fuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 CAPTION = os.environ.get("CAPTION", "")
+
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
 AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 PORT = int(os.environ.get("PORT", "8081"))
-LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002145984196)
+LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1001876507111)
